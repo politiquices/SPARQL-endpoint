3f7123f864f6487e5814de38ff0c180ec6a96e92
David S. Batista <david.batista@comtravo.com>
2022-01-31 00:09:22 +0100

    wip

---
+++ get_entities_names.py
@@ -0,0 +1,62 @@
+import sys
+from os import walk
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+from nlp_extraction.utils.utils import just_sleep, write_iterator_to_file
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+def get_names():
+    filenames = []
+    for _, _, f_names in walk("wiki_ttl"):
+        filenames.extend(f_names)
+
+    batch_size = 400
+    all_results = []
+
+    for ids_batch in batch(filenames, batch_size):
+        wiki_ids = ' '.join(['wd:' + f.split(".")[0] for f in ids_batch])
+        print(f"Downloading {len(ids_batch)} names")
+
+        query = f"""
+            SELECT DISTINCT ?item ?label ?alternative {{
+              VALUES ?item {{{wiki_ids}}}
+              ?item wdt:P31 wd:Q5.      
+              ?item rdfs:label ?label . FILTER(LANG(?label) = "pt")
+              OPTIONAL {{
+                ?item skos:altLabel ?alternative . FILTER(LANG(?alternative) = "pt") 
+              }}
+            }}
+            ORDER BY ?label
+            """
+
+        just_sleep(5, verbose=False)
+        results = get_results(endpoint_url, query)
+        all_results.extend(results["results"]["bindings"])
+
+    return all_results
+
+
+def main():
+    all_results = get_names()
+    write_iterator_to_file([x['label']['value'] for x in all_results], 'entities_names.txt')
+
+
+if __name__ == '__main__':
+    main()
